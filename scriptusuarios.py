2d6e649cb6282bef46a4b27ff874157876d930c6
Daniel Lozano <47381677+DanielLozano21@users.noreply.github.com>
2022-01-21 13:58:52 -0600

    Add files via upload

---
+++ scriptusuarios.py
@@ -0,0 +1,13 @@
+import os
+
+nombre = str(input("Ingresa el nombre del usuario \n"))
+password = str(input("Ingresa la contraseña \n"))
+
+adduser = "useradd -m " + nombre
+addpass = "passwd " + password
+
+
+
+
+
+
